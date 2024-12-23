ed4f47aa022455fe56e932c37928d57bb5322ac9
SanqDuonq <namsang0902s@gmail.com>
2024-12-23 07:06:10 +0700

    Component

---
+++ components/subject.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+const SubjectComponent = () => {
+    return (
+        <View>
+            <Text>
+                I Love React Native !
+            </Text>
+        </View>
+    )
+}
+
+export default SubjectComponent;
---
+++ components/x.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native"
+
+const XComponent = () => {
+    return (
+        <View>
+            <Text className="text-red-500">
+                Here is my GitHub handle (@handle)
+            </Text>
+        </View>
+    )
+}
+
+export default XComponent;
